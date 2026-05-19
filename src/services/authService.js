361efae0cb92ce5b4e21433dd5828a51fee6b954
samuel <mwausamuel33@gmail.com>
2026-05-19 10:32:01 +0300

    AFE-5 Work on the services folder

---
+++ src/services/authService.js
@@ -0,0 +1,10 @@
+import api from './api'
+
+// login
+export const loginUser = (email, password) => api.post('/auth/login', { email, password })
+
+// get current logged in user
+export const getCurrentUser = () => api.get('/auth/me')
+
+// register new user
+export const registerUser = (data) => api.post('/auth/register', data)
